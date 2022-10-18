fb8dec3cf4f3c329deee57032f32967d6649820e
pasandevin <jayawardene.pasandevin@gmail.com>
2022-10-18 19:05:07 +0530

    Initial commit

---
+++ app/src/main/java/com/pasandevin/android/android_mvvm_metadata_viewer/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.pasandevin.android.android_mvvm_metadata_viewer.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
